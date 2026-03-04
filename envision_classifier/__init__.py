e0c214fabab9eb09954f3115ac830f17213b82d2
Jamey ONeill <jimnoneill@gmail.com>
2026-03-03 19:54:44 -0800

    feat: initial beta scaffold with SetFit classifier, CLI, and CI/CD

--- envision_classifier/__init__.py
+++ envision_classifier/__init__.py
@@ -14,7 +14,7 @@ Usage:
     {'label': 'EYE_IMAGING', 'confidence': 0.999, 'probabilities': {...}}
 """
 
-__version__ = "0.1.0-beta.1"
+__version__ = "0.1.0"
 __author__ = "James O'Neill"
 
 from .classifier import EyeImagingClassifier, LABELS
